4869dee054f0bc0fee80a089beb308b265472497
eublefar <evil.unicorn1@gmail.com>
2024-03-01 21:39:02 +0100

    Update demo script to new messages API and show off multiple chat session switching

--- Assets/LLamaSharpTestScript.cs
+++ Assets/LLamaSharpTestScript.cs
@@ -1,33 +1,42 @@
 using UnityEngine;
 using System.Collections.Generic;
-using System;
-using System.Linq;
 using LLama;
-using LLama.Native;
 using LLama.Common;
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine.UI;
+using System.Threading;
+using static LLama.StatefulExecutorBase;
 
 public class LLamaSharpTestScript : MonoBehaviour
 {
     public string ModelPath = "models/mistral-7b-instruct-v0.1.Q4_K_M.gguf"; // change it to your own model path
+    [TextArea(3, 10)]
+    public string SystemPrompt = "Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately and with precision.\r\n\r\nUser: Hello, Bob.\r\nBob: Hello. How may I help you today?\r\nUser: Please tell me the best city in Europe.\r\nBob: Sure. The best city in Europe is Kyiv, the capital of Ukraine.\r\nUser:";
     public TMP_Text Output;
     public TMP_InputField Input;
+    public TMP_Dropdown SessionSelector;
     public Button Submit;
 
+
+    private ExecutorBaseState _emptyState;
+    private List<ExecutorBaseState> _executorStates = new List<ExecutorBaseState>();
+    private List<ChatSession> _chatSessions = new List<ChatSession>();
+    private int _activeSession = 0;
+
     private string _submittedText = "";
+    private CancellationTokenSource _cts;
 
     async UniTaskVoid Start()
     {
-        Submit.interactable = false;
+        _cts = new CancellationTokenSource();
+        SetInteractable(false);
         Submit.onClick.AddListener(() =>
         {
             _submittedText = Input.text;
             Input.text = "";
         });
         Output.text = "User: ";
-        var bobPrompt = "Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately and with precision.\r\n\r\nUser: Hello, Bob.\r\nBob: Hello. How may I help you today?\r\nUser: Please tell me the largest city in Europe.\r\nBob: Sure. The largest city in Europe is Moscow, the capital of Russia.\r\nUser:"; // use the "chat-with-bob" prompt here.
         // Load a model
         var parameters = new ModelParams(Application.streamingAssetsPath + "/" + ModelPath)
         {
@@ -42,15 +51,47 @@ public class LLamaSharpTestScript : MonoBehaviour
         // Initialize a chat session
         using var context = model.CreateContext(parameters);
         var ex = new InteractiveExecutor(context);
-        ChatSession session = new ChatSession(ex);
-
-        Submit.interactable = true;
-        // run the inference in a loop to chat with LLM
-        while (bobPrompt != "stop")
+        // Save the empty state for cases when we need to switch to empty session
+        _emptyState = ex.GetStateData();
+        foreach(var option in SessionSelector.options)
         {
+            var session = new ChatSession(ex);
+            // This won't process the system prompt until the first user message is received
+            // to pre-process it you'd need to look into context.Decode() method.
+            // Create an issue on github if you need help with that.
+            session.AddSystemMessage(SystemPrompt);
+            _chatSessions.Add(session);
+            _executorStates.Add(null);
+        }
+        SessionSelector.onValueChanged.AddListener(SwitchSession);
+        _activeSession = 0;
+        // run the inference in a loop to chat with LLM
+        await ChatRoutine(_cts.Token);
+        Submit.onClick.RemoveAllListeners();
+    }
+
+    /// <summary>
+    /// Chat routine that sends user messages to the chat session and receives responses.
+    /// </summary>
+    /// <param name="session">Active chat session</param>
+    /// <param name="cancel">Cancellation token to stop the routine</param>
+    /// <returns></returns>
+    public async UniTask ChatRoutine(CancellationToken cancel = default)
+    {
+        var userMessage = "";
+        while (!cancel.IsCancellationRequested)
+        {
+            // Allow input and wait for the user to submit a message or switch the session
+            SetInteractable(true);
+            await UniTask.WaitUntil(() => _submittedText != "");
+            userMessage = _submittedText;
+            _submittedText = "";
+            Output.text += " " + userMessage + "\n";
+            // Disable input while processing the message
+            SetInteractable(false);
             await foreach (var token in ChatConcurrent(
-                session.ChatAsync(
-                    bobPrompt, 
+                _chatSessions[_activeSession].ChatAsync(
+                    new ChatHistory.Message(AuthorRole.User, userMessage),
                     new InferenceParams() 
                     { 
                         Temperature = 0.6f, 
@@ -60,13 +101,59 @@ public class LLamaSharpTestScript : MonoBehaviour
             ))
             {
                 Output.text += token;
+                await UniTask.NextFrame();
             }
-            await UniTask.WaitUntil(() => _submittedText != "");
-            bobPrompt = _submittedText;
-            _submittedText = "";
-            Output.text += " " + bobPrompt+"\n";
         }
-        Submit.onClick.RemoveAllListeners();
+    }
+
+    
+    private void SwitchSession(int index)
+    {
+        SaveActiveSession();
+        SetActiveSession(index);
+    }
+
+    /// <summary>
+    /// Saves the state of the active chat session executor.
+    /// </summary>
+    private void SaveActiveSession()
+    {
+        _executorStates[_activeSession] = (_chatSessions[_activeSession].Executor as InteractiveExecutor).GetStateData();
+    }
+
+    /// <summary>
+    /// Sets the active chat session and loads its state.
+    /// If the session has a saved state, it loads it. Otherwise, it loads an empty state.
+    /// </summary>
+    /// <param name="index"></param>
+    private void SetActiveSession(int index)
+    {
+        _activeSession = index;
+        if (_executorStates[_activeSession] != null)
+        {
+            (_chatSessions[_activeSession].Executor as InteractiveExecutor).LoadState(_executorStates[_activeSession]);
+        }
+        else
+        {
+            (_chatSessions[_activeSession].Executor as InteractiveExecutor).LoadState(_emptyState);
+        }
+        Output.text = "User: ";
+        foreach (var message in _chatSessions[_activeSession].History.Messages)
+        {
+            // Skip system prompt
+            if (message.AuthorRole != AuthorRole.System)
+            {
+                Output.text += message.Content + "\n";
+            }
+        }
+    }
+
+    /// <summary>
+    /// Cancels the chat routine when the object is destroyed.
+    /// </summary>
+    private void OnDestroy()
+    {
+        _cts.Cancel();
     }
 
     /// <summary>
@@ -81,5 +168,16 @@ public class LLamaSharpTestScript : MonoBehaviour
         {
             yield return token;
         }
+    }
+
+    /// <summary>
+    /// Sets the interactable property of the UI elements.
+    /// </summary>
+    /// <param name="interactable"></param>
+    private void SetInteractable(bool interactable)
+    {
+        Submit.interactable = interactable;
+        Input.interactable = interactable;
+        SessionSelector.interactable = interactable;
     }
 }
